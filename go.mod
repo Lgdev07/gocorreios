5cbae85b7d9794ed3208e20f9b4d5bc33880a70d
Lgdev07 <luan_saopaulo06@hotmail.com>
2020-10-24 13:25:09 -0300

    [NEW] Add tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Lgdev07/gocorreios
 
 go 1.14
 
-require github.com/tidwall/gjson v1.6.1
+require (
+	github.com/stretchr/testify v1.6.1
+	github.com/tidwall/gjson v1.6.1
+)
---
+++ tracking_test.go
@@ -0,0 +1,83 @@
+package gocorreios
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	codeNumber          string = "OK816158697BR"
+	codeNotFoundMessage string = "ERRO: Objeto não encontrado na base de dados dos Correios."
+)
+
+func TestSearchCodeExists(t *testing.T) {
+	response, err := searchCode(codeNumber)
+	if err != nil {
+		t.Errorf("Error while getting searchCode: %v", err)
+	}
+	assert.Contains(t, string(response), codeNumber)
+}
+
+func TestSearchCodeNotExist(t *testing.T) {
+	response, err := searchCode("WrongCode")
+	if err != nil {
+		t.Errorf("Error while getting searchCode: %v", err)
+	}
+	assert.Contains(t, string(response), codeNotFoundMessage)
+}
+
+func TestTracking(t *testing.T) {
+	response, err := Tracking([]string{codeNumber})
+	if err != nil {
+		t.Errorf("Error while getting Tracking: %v", err)
+	}
+
+	responseInterface := []map[string]interface{}{}
+	err = json.Unmarshal(response, &responseInterface)
+	if err != nil {
+		t.Errorf("Cannot convert to json: %v", err)
+	}
+
+	assert.Equal(t, len(responseInterface), 1)
+	assert.Equal(t, responseInterface[0]["number"], codeNumber)
+	assert.Contains(t, responseInterface[0], "last_date")
+	assert.Contains(t, responseInterface[0], "category")
+	assert.Contains(t, responseInterface[0], "last_status")
+	assert.Contains(t, responseInterface[0], "last_detail")
+	assert.Contains(t, responseInterface[0], "last_origin")
+	assert.Contains(t, responseInterface[0], "last_destination")
+	assert.Contains(t, responseInterface[0], "history")
+}
+
+func TestTrackingNotExist(t *testing.T) {
+	response, err := Tracking([]string{"Wrong number"})
+	if err != nil {
+		t.Errorf("Error while getting Tracking: %v", err)
+	}
+
+	responseInterface := []map[string]interface{}{}
+	err = json.Unmarshal(response, &responseInterface)
+	if err != nil {
+		t.Errorf("Cannot convert to json: %v", err)
+	}
+
+	assert.Equal(t, len(responseInterface), 1)
+	assert.Equal(t, responseInterface[0]["category"], codeNotFoundMessage)
+}
+
+func TestTrackingMultipleObjects(t *testing.T) {
+	response, err := Tracking([]string{codeNumber, codeNumber})
+	if err != nil {
+		t.Errorf("Error while getting Tracking: %v", err)
+	}
+
+	responseInterface := []map[string]interface{}{}
+	err = json.Unmarshal(response, &responseInterface)
+	if err != nil {
+		t.Errorf("Cannot convert to json: %v", err)
+	}
+
+	assert.Equal(t, len(responseInterface), 2)
+}
